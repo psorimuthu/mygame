1f4678023bd80e5631eeb41e9ade702b1bfd93cd
Paddy <sbpaddy@gmail.com>
2020-04-20 03:22:57 -0400

    Third commit with Exception Handling

---
+++ src/main/java/com/pad/mysql/demo/util/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.pad.mysql.demo.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = ProductNotFoundException.class)
+    public ResponseEntity<Object> ProductExceptionHandler(){
+        return new ResponseEntity<>("Product Not found", HttpStatus.NOT_FOUND);
+    }
+
+}
